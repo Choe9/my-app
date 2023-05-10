3387bef6d3fff0410efb8563e2186be6d992d164
DESKTOP-NSNJVU7\runcj <run1529@gmail.com>
2023-05-10 13:44:07 +0900

    리엑트 price 차트 추가

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,5 +1,115 @@
+import { useQuery } from "react-query";
+import { useOutletContext } from "react-router-dom";
+import { fetchCoinHistory } from "../api";
+import ReactApexChart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
+interface PriceProps {
+  coinId?: string;
+}
+
 function Price() {
-  return <h1>Price</h1>;
+  // const { coinId } = useOutletContext() as ChartProps;
+  const { isLoading, data } = useQuery<IHistorical[]>(
+    ["ohlcv", "btc-bitcoin"],
+    // ["ohlcv", coinId],
+    // () => fetchCoinHistory(coinId),
+    () => fetchCoinHistory("btc-bitcoin"),
+    {
+      refetchInterval: 10000,
+    }
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ReactApexChart
+          type="bar"
+          series={[
+            {
+              name: "LowPrice",
+              data: data?.map((price) => Number(price.low)) as number[],
+            },
+            {
+              name: "HighPrice",
+              data: data?.map((price) => Number(price.high)) as number[],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              // height: 300,
+              // width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "rgba(0, 0, 0, 0.5)",
+              stacked: true,
+              stackType: "normal",
+            },
+            // grid: { show: false },
+            // stroke: {
+            //   curve: "smooth",
+            //   width: 4,
+            // },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+            },
+            // fill: {
+            //   type: "gradient",
+            //   gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            // },
+            // colors: ["#0fbcf9"],
+            // tooltip: {
+            //   y: {
+            //     formatter: (value) => `$ ${value.toFixed(3)}`,
+            //   },
+            // },
+            colors: ["#FF4560", "#008FFB"],
+            plotOptions: {
+              bar: {
+                horizontal: true,
+                barHeight: "70%",
+              },
+            },
+
+            dataLabels: {
+              enabled: true,
+            },
+            // stroke: {
+            //   width: 1,
+            //   colors: ["#fff"],
+            // },
+            grid: {
+              xaxis: {
+                lines: {
+                  show: false,
+                },
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Price;
